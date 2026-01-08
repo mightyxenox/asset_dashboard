dcf213febb5acc1fcbb01c0d7698c8ced2789f66
Developer <developer@example.com>
2026-01-08 06:01:35 +0530

    Update utils.js

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8000';
+const BASE_URL = 'http://asset-dashboard-bcnv.onrender.com';
 
 export const fetchWithAuth = async (path, options = {}) => {
   const token = localStorage.getItem('token');
